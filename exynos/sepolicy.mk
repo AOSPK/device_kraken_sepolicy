6f303262489794fe71b59b1ebe89faf89da1bfe6
Alexandre Rangel <mamutal91@gmail.com>
2022-01-22 02:15:49 -0300

    Fix path
    Signed-off-by: Alexandre Rangel <mamutal91@gmail.com>

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/exynos/private
+    device/kraken/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/exynos/dynamic \
-    device/custom/sepolicy/exynos/system
+    device/kraken/sepolicy/exynos/dynamic \
+    device/kraken/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/custom/sepolicy/exynos/dynamic \
-    device/custom/sepolicy/exynos/vendor
+    device/kraken/sepolicy/exynos/dynamic \
+    device/kraken/sepolicy/exynos/vendor
 endif
